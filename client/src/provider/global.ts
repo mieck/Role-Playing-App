69f939f9746d9829d965134bfbf01252dbac38de
Marie <xxyukiko96@gmail.com>
2018-07-03 17:36:18 +0200

    Testen, ob IP vom PC klappt bei App.
    Wurde auf Locahost zurückgesetzt.

--- client/src/provider/global.ts
+++ client/src/provider/global.ts
@@ -9,6 +9,10 @@ export class GlobalProvider {
 
   public otherCharID:String;
 
+  //IP Marie PC cmd -> ipconfig -> IPv4 Adresse
+  //public serverHost:String ="http://192.168.0.20:8080";
+
+  //Local Host
   public serverHost:String = "http://localhost:8080";
 
   public isAdmin:boolean;
